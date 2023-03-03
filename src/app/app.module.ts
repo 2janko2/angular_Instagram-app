b85f878cd239033210899ebd3df6fc438dc62e94
Ivan Khlysta <ivan.khlysta@gmail.com>
2023-03-03 16:42:57 +0200

    Add first Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestComponent } from './testComponents/test.component';
+
+// Modules are separate complete pages which we cannot reuse somewhere else
+// We need to create shared modules for reused components then
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent
+    // here we should write out components which we use in our app
   ],
   imports: [
     BrowserModule,
