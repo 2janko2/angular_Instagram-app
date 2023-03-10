f120281c61b3e75b61330e774b0c57268ed79e5f
Ivan Khlysta <ivan.khlysta@gmail.com>
2023-03-10 19:43:28 +0200

    Finish creating markup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './testComponents/header.component';
+import { HeaderComponent } from './components/header.component';
 import { SecondTestComponent } from './second-test/second-test.component';
+import { VacancyCardComponent } from './vacancy-card/vacancy-card.component';
 
 // Modules are separate complete pages which we cannot reuse somewhere else
 // We need to create shared modules for reused components then
@@ -13,7 +14,8 @@ import { SecondTestComponent } from './second-test/second-test.component';
   declarations: [
     AppComponent,
     HeaderComponent,
-    SecondTestComponent
+    SecondTestComponent,
+    VacancyCardComponent
     // here we should write out components which we use in our app
   ],
   imports: [
---
+++ src/app/vacancy-card/vacancy-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'inst-vacancy-card',
+  templateUrl: './vacancy-card.component.html',
+  styleUrls: ['./vacancy-card.component.scss']
+})
+export class VacancyCardComponent {
+
+}
