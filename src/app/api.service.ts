f7baa121f65890a3e68f6cb6a40ea20e8bdb2d90
Ivan Khlysta <ivan.khlysta@gmail.com>
2023-03-16 14:57:51 +0100

    Envs

---
+++ src/app/api.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.development';
+
+
+export interface StandardResponse {
+  addedDate: string,
+  id: string,
+  order: string,
+  title: string
+}
+
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ApiService {
+  requestOptions = {
+    withCredentials: true,
+    headers: {
+      'api-key': environment.apiKey
+    }
+  }
+
+  constructor(private http: HttpClient) {
+  }
+
+  getList(): Observable<StandardResponse[]> {
+    return this.http.get<StandardResponse[]>(`${environment.baseUrl}/todo-lists`, this.requestOptions)
+  }
+}
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    baseUrl: 'https://social-network.samuraijs.com/api/1.1',
+    apiKey: '35c71d6c-4204-45d4-8704-3f88d800f4b0'
+};
